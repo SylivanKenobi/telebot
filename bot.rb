727b15840963cb97e39b4346359ec0def5ee08ef
Sylvain <sypsilon@hotmail.com>
2019-08-24 21:08:50 +0200

    First basic bot

---
+++ bot.rb
@@ -0,0 +1,23 @@
+require 'telegram_bot'
+
+token ='961383937:AAE4iNclJMUyzkEmZjcF8aWaTFuz-PXuc4U'
+
+bot = TelegramBot.new(token: token)
+
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+
+  message.reply do |reply|
+    case command
+    when /start/i
+      reply.text = "All I can do is say hello. Try the /greet command."
+    when /greet/i
+      reply.text = "Hello, #{message.from.first_name}. 🤖"
+    else
+      reply.text = "I have no idea what #{command.inspect} means."
+    end
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
+  end
+end
