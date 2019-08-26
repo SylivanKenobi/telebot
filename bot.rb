3f1ccc7dca79db9569db459f7245006d41b94b02
Sylvain <sypsilon@hotmail.com>
2019-08-26 22:15:55 +0200

    Make bot work

--- bot.rb
+++ bot.rb
@@ -5,21 +5,31 @@ require 'csv'
 
 token ='961383937:AAE4iNclJMUyzkEmZjcF8aWaTFuz-PXuc4U'
 
+re1='.*?'	# Non-greedy match on filler
+re2='(?:[a-z][a-z]+)'	# Uninteresting: word
+re3='.*?'	# Non-greedy match on filler
+re4='((?:[a-z][a-z]+))'	# Word 1
+re=(re1+re2+re3+re4)
+m=Regexp.new(re,Regexp::IGNORECASE);
 bot = TelegramBot.new(token: token)
+result = ""
 
 bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
 
-  message.reply do |reply|
+    message.reply do |reply|
     case command
-    when command
-      data = CSV.parse(message.text)
-      resString = JSON.parse(URI.parse("https://api.datamuse.com/words?rel_rhy=#{data[0][0]}&max=#{data[0][1]}").read)
-      puts message.text
-      reply.text = "#{resString}"
+    when /start/i
+      reply.text = "All I can do is rhyme. Sent me a word I'll rhyme it."
     else
-      reply.text = "I have no idea what #{command.inspect} means."
+      data = CSV.parse(message.text)
+      resString = JSON.parse(URI.parse("https://api.datamuse.com/words?rel_rhy=#{data[0][0]}&max=5").read)
+      resString.each do |i|
+        puts i["word"]
+        result += i["word"] +  "\n"
+      end
+      reply.text = "#{result}"
     end
     puts "sending #{reply.text.inspect} to @#{message.from.username}"
     reply.send_with(bot)
